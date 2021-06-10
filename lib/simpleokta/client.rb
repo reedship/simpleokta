a3d530fe772d7b4e68408642cfe6f5710c208e32
Braden Shipley <bradenrshipley@gmail.com>
2021-06-10 12:54:38 -0500

    removed schemas. testing will be done through vcrs

---
+++ lib/simpleokta/client.rb
@@ -0,0 +1,43 @@
+module Simpleokta
+  class Client
+    require 'faraday'
+    require 'json'
+    require 'erb'
+    include Apps
+    include AuthServers
+    include Groups
+    include Constants
+    include SystemLogs
+    include Users
+
+    attr_accessor :api_token, :base_api_url
+
+    # Initialize using passed in config hash
+    # @param config [Hash]
+    def initialize(config)
+      @api_token = config.api_token
+      @base_api_url = config.base_api_url
+    end
+
+    # Setting initial connection with base_api_url from config
+    def connection
+      @conn ||= Faraday.new(@base_api_url)
+    end
+
+    # This method will add our api_token to each authorization header to keep our code D.R.Y
+    # @param action [String] the HTTP verb we are sending our request with.
+    #   IE: 'get', 'post', 'put', 'delete'
+    # @param url [String] the URL to send the request to.
+    # @param body [Hash] the request body, set to an empty hash by default.
+    #   Each request may require a different body schema.
+    def call_with_token(action, url, body={})
+      connection.send(action) do |req|
+        req.url url
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['Accept'] = 'application/json'
+        req.headers['Authorization'] = "SSWS #{@api_token}"
+        req.body = body
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
-require "bundler/setup"
-require "simpleokta"
-require_relative "../lib/simpleokta.rb"
 require "vcr"
 
+module AgentsAccessTokenFilter
+  private
+
+  def serializable_body(*)
+    body = super
+    body['string'].gsub!(/"access_token":\s*"\w+"/, '"access_token": "<<AGENT_ACCESS_TOKEN>>"')
+    body['string'].gsub!(/"ip_address":\s*"[\d\.]+"/, '"ip_address": "127.0.0.1"')
+    body
+  end
+end
+
+VCR::Response.include(AgentsAccessTokenFilter)
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -14,4 +23,31 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
+  VCR.configure do |config|
+    config.configure_rspec_metadata!
+    config.cassette_library_dir = 'spec/cassettes/'
+    config.hook_into(:faraday)
+    config.filter_sensitive_data('<<ACCESS_TOKEN>>') do
+      fake_okta_api_token
+    end
+  end
+
+  config.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
+
+end
+
+
+module TestingClient
+  extend RSpec::SharedContext
+  let(:client) { Simpleokta::Client.new(token: fake_okta_api_token, organization: 'test_org') }
+end
+
+RSpec.configure do |config|
+  config.include(TestingClient)
+end
+
+def fake_okta_api_token
+  ENV.fetch('API_TOKEN', 'x'*40)
 end
