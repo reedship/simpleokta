ebd2cbf2eca540134849f5978be5147a9f2affb9
Braden Shipley <bradenrshipley@gmail.com>
2021-06-14 20:05:49 -0500

    vcr now implemented. will be adding cassettes starting now
    Signed-off-by: Braden Shipley <bradenrshipley@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ RSpec.configure do |config|
   VCR.configure do |config|
     config.configure_rspec_metadata!
     config.cassette_library_dir = 'spec/cassettes/'
-    config.hook_into(:faraday)
+    config.hook_into(:webmock)
     config.filter_sensitive_data('<<ACCESS_TOKEN>>') do
       fake_okta_api_token
     end
